0cca333a28c39dc290ce0a70e0a7369ee2495de5
filipszoldra <37276160+filipszoldra@users.noreply.github.com>
2025-07-15 20:05:40 +0200

    delay model fix

--- dbt/gtfs_project/models/delays_table.sql
+++ dbt/gtfs_project/models/delays_table.sql
@@ -1,18 +1,6 @@
-{{ config(materialized='incremental', alias='DELAYS_TABLE')}}
+{{ config(materialized='table', alias='DELAYS_TABLE')}}
 
-{% if is_incremental() %}
-    with last_run as (
-        select max(load_timestamp) as max_ts
-        from {{ this }}
-    ),
-{% else %}
-    with last_run as (
-        select cast('1900-01-01' as timestamp) as max_ts
-    ),
-{% endif %}
-
-
-NEXT_STOP AS (
+WITH NEXT_STOP AS (
     SELECT 
         trip_id, 
         stop_id, 
@@ -20,28 +8,29 @@ NEXT_STOP AS (
         MODE, 
         LOAD_TIMESTAMP,
         TO_CHAR(CONVERT_TIMEZONE('UTC','Europe/Warsaw', to_timestamp_ntz(IFNULL(arrival, departure)::NUMBER))::DATE, 'YYYYMMDD') AS EVENT_DATE
-    FROM gtfs_test.trip_updates.trip_updates 
-    WHERE load_timestamp > (SELECT max_ts from last_run)
+    FROM {{ source('trip_updates', 'trip_updates') }} 
+        QUALIFY ROW_NUMBER() OVER (PARTITION BY TRIP_ID, MODE, STOP_ID, EVENT_DATE ORDER BY LOAD_TIMESTAMP DESC) = 1
 ), JOINED_WITH_SCHEDULE AS (
 SELECT 
-    N.TRIP_ID, 
-    N.STOP_ID,
+    S.TRIP_ID, 
+    S.STOP_ID,
     S.STOP_NAME,
     S.TRIP_HEADSIGN,
     S.STOP_SEQUENCE,
     N.ACTUAL_ARRIVAL,
     S.ROUTE_SHORT_NAME,
     TO_TIMESTAMP(TO_DATE(N.ACTUAL_ARRIVAL) || ' ' || S.ARRIVAL_TIME || '.000') as PLANNED_ARRIVAL,
-    N.MODE,
-    N.EVENT_DATE,
+    S.MODE,
+    S.EVENT_DATE,
     N.LOAD_TIMESTAMP,
     S.LOAD_TIMESTAMP AS SCHEDULE_TIMESTAMP
 FROM NEXT_STOP as N 
-RIGHT JOIN {{ ref('trips_schedule_table') }} AS S
+JOIN {{ ref('trips_schedule_table') }} AS S
     ON N.trip_id = S.trip_id 
     and N.MODE=S.MODE 
     and N.stop_id=S.stop_id 
     and N.event_date = S.event_date
+    --and S.LOAD_TIMESTAMP <= N.LOAD_TIMESTAMP
 QUALIFY ROW_NUMBER() 
         OVER (PARTITION BY S.trip_id, S.stop_id, S.MODE, S.event_date ORDER BY S.LOAD_TIMESTAMP DESC) = 1
 ), delays as (
