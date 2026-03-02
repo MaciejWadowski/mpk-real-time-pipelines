5ac235f0c9a8d9ac30482cb5c7a3e31ec2143b69
Maciej Wadowski <macwadowskii@gmail.com>
2026-03-02 20:26:58 +0100

    Example query

---
+++ notebooks/stop_paris_delay.sql
@@ -0,0 +1,107 @@
+WITH
+    latest_trip_update AS (
+        -- keep the row with the largest delay per (trip_id, stop_sequence, mode) - case when trip prolongs longer than 1 minute
+        SELECT
+            trip_id,
+            stop_sequence,
+            stop_id,
+            mode,
+            delay,
+            load_timestamp
+        FROM
+            GTFS_TEST.SCHEDULE_DATA_MARTS.DELAYS_TABLE QUALIFY ROW_NUMBER() OVER (
+                PARTITION BY
+                    trip_id,
+                    stop_sequence,
+                    mode
+                ORDER BY
+                    load_timestamp DESC,
+                    delay DESC
+            ) = 1
+    ),
+    paired AS (
+        -- pair each stop with the next stop on the same trip and same mode
+        SELECT
+            trip_id,
+            mode,
+            stop_sequence,
+            stop_id,
+            delay,
+            load_timestamp,
+            LEAD(stop_id) OVER (
+                PARTITION BY
+                    trip_id,
+                    mode
+                ORDER BY
+                    stop_sequence
+            ) AS next_stop_id,
+            LEAD(stop_sequence) OVER (
+                PARTITION BY
+                    trip_id,
+                    mode
+                ORDER BY
+                    stop_sequence
+            ) AS next_stop_sequence
+        FROM
+            latest_trip_update
+    ),
+    valid_pairs AS (
+        -- keep only real consecutive pairs (both sides exist) and origin before dest
+        SELECT
+            stop_id,
+            next_stop_id,
+            delay,
+            DATE_PART('hour', load_timestamp) AS delay_hour,
+            TO_DATE(load_timestamp) AS delay_date
+        FROM
+            paired
+        WHERE
+            next_stop_id IS NOT NULL -- remove if there is no last stop
+            AND stop_sequence + 1 = next_stop_sequence -- remove if we have a gap between stations
+    ),
+    avg_delays AS (
+        SELECT
+            COUNT(1) AS cnt,
+            AVG(delay) AS avg_delay,
+            stop_id,
+            next_stop_id,
+            delay_hour,
+            delay_date
+        FROM
+            valid_pairs
+        GROUP BY
+            stop_id,
+            next_stop_id,
+            delay_hour,
+            delay_date
+    ),
+-- enrich with geolocation, if not needed it could be removed
+    avg_delays_geolocation AS (
+        SELECT
+            a.cnt,
+            a.avg_delay,
+            a.stop_id,
+            b.stop_lat,
+            b.stop_lon,
+            a.next_stop_id,
+            c.stop_lat AS next_stop_lat,
+            c.stop_lon AS next_stop_lon,
+            a.delay_hour,
+            a.delay_date
+        FROM
+            avg_delays a
+            LEFT JOIN GTFS_TEST.SCHEDULE.STOPS b ON a.stop_id = b.stop_id
+            AND a.delay_date = TO_DATE(b.load_timestamp)
+            LEFT JOIN GTFS_TEST.SCHEDULE.STOPS c ON a.stop_id = c.stop_id
+            AND a.delay_date = TO_DATE(c.load_timestamp)
+    )
+-- example query usage
+SELECT
+    *
+FROM
+    avg_delays_geolocation
+WHERE
+    delay_date = '2026-02-25'
+ORDER BY
+    stop_id DESC,
+    delay_hour;
